af8118bfe6e8edc77c288576bc93432c065bb5c7
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-19 22:30:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,6 @@
 # Make your shoe class here!
 class Shoe
+  attr_accessor :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
@@ -9,37 +10,6 @@ class Shoe
     @brand
   end
 
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size = size
-  end
-
-  def size
-    @size
-  end
-
-  def material=(material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
-
-  def condition=(condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
-  end
 
   def cobble
     @condition = "new"
