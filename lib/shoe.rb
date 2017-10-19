9bcca5f6e5fa5db533c2a5eaaf697fa7c3118509
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-19 22:14:47 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,20 @@
 # Make your shoe class here!
 class Shoe
+
   def initialize(brand)
     @brand = brand
   end
 
   def brand
     @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
   end
 
 end
